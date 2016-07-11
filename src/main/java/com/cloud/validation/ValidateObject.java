44751339ec22772b255dcecd44f5f85c06076c83
kolr <kol-radik@yandex.ru>
2016-07-11 21:37:37 +0300

    Commented ValidateObject.

--- src/main/java/com/cloud/validation/ValidateObject.java
+++ src/main/java/com/cloud/validation/ValidateObject.java
@@ -3,6 +3,8 @@ package com.cloud.validation;
 /**
  * 11.07.2016
  * Created by Rodion.
+ * <p>
+ * Class of the object, that system going to validate, must inherit this abstract class.
  */
 public abstract class ValidateObject {
     protected FieldTypes type;
