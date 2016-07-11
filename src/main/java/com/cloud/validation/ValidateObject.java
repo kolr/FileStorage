770b08dd84b4edaa6185443b9351cf3336d167f7
kolr <kol-radik@yandex.ru>
2016-07-11 21:31:13 +0300

    Added abstract class for objects to validate

---
+++ src/main/java/com/cloud/validation/ValidateObject.java
@@ -0,0 +1,11 @@
+package com.cloud.validation;
+
+/**
+ * 11.07.2016
+ * Created by Rodion.
+ */
+public abstract class ValidateObject {
+    protected FieldTypes type;
+
+    abstract void setFieldType(FieldTypes type);
+}
