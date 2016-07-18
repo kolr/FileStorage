3a867876bf8c72e93206531ed6626eb28cea39b5
kolr <kol-radik@yandex.ru>
2016-07-18 11:19:46 +0300

    Merge branch 'master' of C:\Users\Rodion\IdeaProjects\FileStorage with conflicts.

--- src/main/java/com/cloud/controllers/UserController.java
+++ src/main/java/com/cloud/controllers/UserController.java
@@ -2,7 +2,7 @@ package com.cloud.controllers;
 
 import com.cloud.entities.User;
 import com.cloud.entities.beans.SignInBean;
-import com.cloud.services.UserService;
+import com.cloud.dao.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,9 +31,9 @@ public class UserController {
         String pass = request.getParameter("pass");
         SignInBean credentials = new SignInBean(email, pass);
         LOGGER.info(credentials);
-        User user = userService.getUser(credentials);
+        User user = userService.get(Integer.valueOf(credentials.getEmail()));
         if (user != null) {
-            if (user.getPass().equals(pass)) {
+            if (user.getPassword().equals(pass)) {
                 model.addAttribute("currentUser", user);
             } else {
                 model.addAttribute("errorMessage", "Login or Password was entered not correctly.");
@@ -42,20 +42,20 @@ public class UserController {
         return "acc";
     }
 
-    @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registration(HttpServletRequest request, Model model) {
-        User newUser = generateUser(request);
-        userService.addUser(newUser);
-        model.addAttribute("lst", userService.getALl());
-        return "users";
-    }
+//    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+//    public String registration(HttpServletRequest request, Model model) {
+//        User newUser = generateUser(request);
+//        userService.addUser(newUser);
+//        model.addAttribute("lst", userService.getALl());
+//        return "users";
+//    }
 
     private User generateUser(HttpServletRequest request) {
         User user = new User();
         user.setEmail(request.getParameter("login"));
         user.setName(request.getParameter("name"));
         user.setLastName(request.getParameter("lastName"));
-        user.setPass(request.getParameter("pass"));
+        user.setPassword(request.getParameter("pass"));
         return user;
     }
 
---
+++ src/main/java/com/cloud/dao/UserService.java
@@ -0,0 +1,53 @@
+package com.cloud.dao;
+
+import com.cloud.entities.User;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Query;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import javax.annotation.Resource;
+import javax.jws.soap.SOAPBinding;
+import java.util.List;
+
+/**
+ * Created by nikit on 13.07.2016.
+ */
+
+@Service("userService")
+@Transactional
+public class UserService {
+
+    private static final Logger LOGGER = Logger.getLogger(UserService.class);
+
+    @Resource(name = "sessionFactory")
+    private SessionFactory sessionFactory;
+
+    public List<User> getAll(){
+        LOGGER.debug("Retrieving all persons");
+        Session session = sessionFactory.getCurrentSession();
+
+        Query query = session.createQuery("FROM User");
+        return query.list();
+        //для обновления
+    }
+
+    public User get(int id){
+        Session session = sessionFactory.getCurrentSession();
+        User user = (User) session.get(User.class, id);
+        return user;
+    }
+
+    public void add(User user){
+        Session session = sessionFactory.getCurrentSession();
+        session.save(user);
+    }
+
+    public void delete(String email){
+        Session session = sessionFactory.getCurrentSession();
+
+        User user = (User) session.get(User.class, email);
+        session.delete(user);
+    }
+}
